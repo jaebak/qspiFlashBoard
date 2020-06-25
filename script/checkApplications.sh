9a63fe9e9c8686430560abd9436309b9059097df
Jaebak <jbkim@ucsb.edu>
2020-06-25 17:40:36 +0000

    Added check Application

---
+++ script/checkApplications.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo 'Kill the below running programms'
+ps aux | grep readAndValidate | grep -v grep
+ps aux | grep checkProm | grep -v grep
+ps aux | grep checkUsb | grep -v grep
+ps aux | grep eraseProm | grep -v grep
+ps aux | grep readAndValidate | grep -v grep
+ps aux | grep writeProm | grep -v grep
